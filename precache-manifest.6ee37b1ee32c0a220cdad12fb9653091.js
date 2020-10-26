a4d2082eef9ae4ec8b9c7b7287f8c9afbd446ae7
Deepak3211 <deepakt754@gmail.com>
2020-10-27 01:37:34 +0530

    Updates

---
+++ precache-manifest.6ee37b1ee32c0a220cdad12fb9653091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c4662f60f149531ff7543cfa77fb24",
+    "url": "/theme-toggler/index.html"
+  },
+  {
+    "revision": "f169c69563d6ee6f75ab",
+    "url": "/theme-toggler/static/css/main.855b41ad.chunk.css"
+  },
+  {
+    "revision": "145c9256048ca33c1f37",
+    "url": "/theme-toggler/static/js/2.16c59845.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/theme-toggler/static/js/2.16c59845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f169c69563d6ee6f75ab",
+    "url": "/theme-toggler/static/js/main.f782a34e.chunk.js"
+  },
+  {
+    "revision": "0a5c53c33b65f5a7c116",
+    "url": "/theme-toggler/static/js/runtime-main.fad457f1.js"
+  }
+]);
