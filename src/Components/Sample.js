7589b267d9427f555d009e6d8ff5631f70114f66
Deepak3211 <deepakt754@gmail.com>
2020-10-27 02:09:44 +0530

    changing style

--- src/Components/Sample.js
+++ src/Components/Sample.js
@@ -5,6 +5,7 @@ import AppTheme from '../Color';
 const Sample = () => {
   const theme = useContext(ThemeContext)[0];
   const currentTheme = AppTheme[theme];
+  const [themeMode, setThemeMode]  = useContext(ThemeContext)
 
   return (
     <div style = {{
@@ -16,10 +17,13 @@ const Sample = () => {
       
       <h1>Context Api theme toggler</h1>
       <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur repellendus dolores qui unde facilis perspiciatis neque nisi, excepturi aspernatur eveniet voluptas voluptate dignissimos possimus ipsum omnis incidunt suscipit consequuntur ipsam?</p>
-      <button style = {{
+      <button onClick = {()=>{
+        setThemeMode(themeMode === 'light' ? 'dark': 'light')
+      }} style = {{
         backgroundColor: 'aquamarine',
         padding : '10px 40px',
         fontSize: '15px',
+        cursor:'pointer',
         color: 'whitesmoke',
         border: `${currentTheme.border}`
       }}>Click me</button>
